c6451d29034f3fda90c2ba792fca41ebed90c7a8
Alessandro Corbetta <corbisoft@gmail.com>
2021-09-13 15:52:21 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("Readme.md", "r") as fh:
 
 setup(name='sociophysicsDataHandler',
       description="Data retriever package for the Sociophysics course at TU/Eindhoven.",
-      version='1.0.4',
+      version='1.1.0',
       author='Alessandro Corbetta; Cas Pouw; Joris Willems',
       author_email='a.corbetta@tue.nl',
       url='https://github.com/acorbe/sociophysicsDataHandler',
